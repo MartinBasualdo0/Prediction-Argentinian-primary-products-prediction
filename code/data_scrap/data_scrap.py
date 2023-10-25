4bfb118ee7228b2fd8ea465012f2c46a0870a7ba
Martín Basualdo <70729951+MartinBasualdo0@users.noreply.github.com>
2023-10-25 14:35:38 -0300

    begin of webscraping

---
+++ code/data_scrap/data_scrap.py
@@ -0,0 +1,11 @@
+# import code.data_scrap.init_webdriver as web
+from code.data_scrap.init_webdriver.init_webdriver import inicio_driver, scrap_link_xls
+import os
+import sys
+
+
+if __name__ == "__main__":
+    print("ok")
+    print(sys.path)
+    # print("osd",os.getcwd())
+    # web.inicio_driver("","")
