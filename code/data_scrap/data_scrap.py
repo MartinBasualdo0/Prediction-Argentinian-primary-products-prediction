16493ed9895f5330fe95b5030689e15306a2a736
Martín Basualdo <70729951+MartinBasualdo0@users.noreply.github.com>
2023-10-25 17:02:14 -0300

    scrap ica web

--- code/data_scrap/data_scrap.py
+++ code/data_scrap/data_scrap.py
@@ -1,8 +1,11 @@
 # from init_webdriver.init_webdriver import inicio_driver, scrap_link_xls
-from variables.productos_primarios import scrap_excels_expo_ica_digital
+from variables.excels_ica_digital import scrap_excels_ica_digital
+from init_webdriver.download_folder import download_primary_products,download_pp_price_index
 import os
 import sys
 
 
 if __name__ == "__main__":
-    scrap_excels_expo_ica_digital(timeout=10, replace=True)
+    scrap_excels_ica_digital(xlsx_end_point="serie_rubros_usos_mensual.xlsx", download_folder=download_primary_products,timeout=10, replace=True)
+    scrap_excels_ica_digital(xlsx_end_point="indices_expo.xlsx", download_folder=download_pp_price_index,timeout=10, replace=True)
+    
--- code/data_scrap/init_webdriver/download_folder.py
+++ code/data_scrap/init_webdriver/download_folder.py
@@ -1,2 +1,3 @@
-download_folder="\code\data_scrap\downloads"
-download_primary_products="\code\data_scrap\downloads\primary_products"
+download_folder = "\code\data_scrap\downloads"
+download_primary_products = f"{download_folder}\primary_products"
+download_pp_price_index = f"{download_folder}\pp_price_index"
