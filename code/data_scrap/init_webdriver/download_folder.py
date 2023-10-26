553c858b8c48033d03d42cc408e1fc838b5cd17b
Martín Basualdo <70729951+MartinBasualdo0@users.noreply.github.com>
2023-10-25 22:20:45 -0300

    scrap exogenous

--- code/data_scrap/init_webdriver/download_folder.py
+++ code/data_scrap/init_webdriver/download_folder.py
@@ -1,3 +1,5 @@
 download_folder = "\code\data_scrap\downloads"
 download_primary_products = f"{download_folder}\primary_products"
 download_pp_price_index = f"{download_folder}\pp_price_index"
+download_exchange_rates = f"{download_folder}\exchange_rate"
+download_inflation = f"{download_folder}\exchange_rate\inflation"
---
+++ code/data_scrap/variables/real_exchange_rate.py
@@ -0,0 +1,45 @@
+import yfinance as yf
+import pandas as pd
+
+def get_exchange_rates(download_folder:str) -> pd.DataFrame:
+    acciones = [
+        ('GGAL', 'GGAL.BA', 10),
+        ('YPF', 'YPFD.BA', 1),
+        ('PAM', 'PAMP.BA', 25),
+    ]
+
+    adrs = pd.DataFrame()
+    locales = pd.DataFrame()
+    ccls = pd.DataFrame()
+
+    for accion in acciones:
+        tmp_adr = yf.download(accion[0], period='1d' , start=2004)['Adj Close']
+        tmp_adr.index = tmp_adr.index.tz_localize(None).round("D")
+        adrs[accion[0]] = tmp_adr
+        tmp_loc = yf.download(accion[1], period='1d' , start=2004)['Adj Close']
+        tmp_loc.index = tmp_loc.index.tz_localize(None).round("D")
+        locales[accion[1]] = tmp_loc
+
+        ccls[accion[0]] = locales[accion[1]] * accion[2] / adrs[accion[0]]
+        ccls[accion[0]].interpolate(method='linear',inplace=True)
+
+    ars = yf.download("ARS=X", period='1d' , start=2004)['Adj Close']
+    ars.index = ars.index.tz_localize(None).round("D")
+    ars_mensual = ars.resample("M").mean()
+
+    ccl = ccls.mean(axis=1)
+    ccl_mensual = ccl.resample("M").mean()
+    brecha_bruta = (ccl/ars).ffill().fillna(1)
+    brecha_neta = brecha_bruta - 1
+    brecha_neta_mensual = brecha_neta.resample("M").mean()
+    brecha_bruta_mensual = 1+brecha_neta_mensual
+
+    df = pd.concat([ccl_mensual, ars_mensual, brecha_neta_mensual],axis=1)
+    df.columns = ["dolar_ccl","offic_er","gap"]
+    # print("."+download_folder)
+    df.to_excel("."+download_folder+"/exchange_rates.xlsx")
+    return df
+
+
+
+
