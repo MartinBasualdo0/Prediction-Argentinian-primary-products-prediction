844e6ad6d9b5c02a94524a9eb2d1378f42857091
Martín Basualdo <70729951+MartinBasualdo0@users.noreply.github.com>
2023-11-20 19:39:28 -0300

    too many things

--- code/notebooks/config_notebooks/config.py
+++ code/notebooks/config_notebooks/config.py
@@ -4,4 +4,4 @@ def set_wd():
     sys.path.append(os.path.abspath('../'))
     dir = os.getcwd()
     os.chdir(dir.replace("\\code\\notebooks", ""))
-    return os.getcwd()
+    # return os.getcwd()
