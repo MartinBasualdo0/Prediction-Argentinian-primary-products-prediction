66901a80e88d8f7d9229eef5d4d02d02493a4dc2
Martín Basualdo <martinbasualdo34@gmail.com>
2023-11-21 23:43:27 -0300

    translation

--- code/data_scrap/variables/real_exchange_rate.py
+++ code/data_scrap/variables/real_exchange_rate.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 
 def get_exchange_rates(download_folder:str) -> pd.DataFrame:
-    acciones = [
+    shares = [
         ('GGAL', 'GGAL.BA', 10),
         ('YPF', 'YPFD.BA', 1),
         ('PAM', 'PAMP.BA', 25),
@@ -12,31 +12,30 @@ def get_exchange_rates(download_folder:str) -> pd.DataFrame:
     locales = pd.DataFrame()
     ccls = pd.DataFrame()
 
-    for accion in acciones:
-        tmp_adr = yf.download(accion[0], period='1d' , start=2004)['Adj Close']
+    for share in shares:
+        tmp_adr = yf.download(share[0], period='1d' , start=2004)['Adj Close']
         tmp_adr.index = tmp_adr.index.tz_localize(None).round("D")
-        adrs[accion[0]] = tmp_adr
-        tmp_loc = yf.download(accion[1], period='1d' , start=2004)['Adj Close']
+        adrs[share[0]] = tmp_adr
+        tmp_loc = yf.download(share[1], period='1d' , start=2004)['Adj Close']
         tmp_loc.index = tmp_loc.index.tz_localize(None).round("D")
-        locales[accion[1]] = tmp_loc
+        locales[share[1]] = tmp_loc
 
-        ccls[accion[0]] = locales[accion[1]] * accion[2] / adrs[accion[0]]
-        ccls[accion[0]].interpolate(method='linear',inplace=True)
+        ccls[share[0]] = locales[share[1]] * share[2] / adrs[share[0]]
+        ccls[share[0]].interpolate(method='linear',inplace=True)
 
     ars = yf.download("ARS=X", period='1d' , start=2004)['Adj Close']
     ars.index = ars.index.tz_localize(None).round("D")
-    ars_mensual = ars.resample("M").mean()
+    ars_monthly = ars.resample("M").mean()
 
     ccl = ccls.mean(axis=1)
-    ccl_mensual = ccl.resample("M").mean()
-    brecha_bruta = (ccl/ars).ffill().fillna(1)
-    brecha_neta = brecha_bruta - 1
-    brecha_neta_mensual = brecha_neta.resample("M").mean()
-    brecha_bruta_mensual = 1+brecha_neta_mensual
+    ccl_monthly = ccl.resample("M").mean()
+    gross_gap = (ccl/ars).ffill().fillna(1)
+    net_gap = gross_gap - 1
+    net_gap_monthly = net_gap.resample("M").mean()
+    gross_gap_monthly = 1+net_gap_monthly
 
-    df = pd.concat([ccl_mensual, ars_mensual, brecha_bruta_mensual],axis=1)
+    df = pd.concat([ccl_monthly, ars_monthly, gross_gap_monthly],axis=1)
     df.columns = ["dolar_ccl","offic_er","gap"]
-    # print("."+download_folder)
     df.to_excel("."+download_folder+"/exchange_rates.xlsx")
     return df
 
